ff404d0effbf2addc09ce9ee2c666302a21bdde5
Shaun Sephton <shaunsephton@gmail.com>
2011-08-10 10:45:33 +0200

    use ssl

--- captcha/widgets.py
+++ captcha/widgets.py
@@ -1,6 +1,9 @@
 from django import forms
-from django.utils.safestring import mark_safe
 from django.conf import settings
+from django.utils.safestring import mark_safe
+
+from captcha import utils
+
 from recaptcha.client import captcha
 
 class ReCaptcha(forms.widgets.Widget):
@@ -8,7 +11,7 @@ class ReCaptcha(forms.widgets.Widget):
     recaptcha_response_name = 'recaptcha_response_field'
 
     def render(self, name, value, attrs=None):
-        return mark_safe(u'%s' % captcha.displayhtml(settings.RECAPTCHA_PUBLIC_KEY))
+        return mark_safe(u'%s' % captcha.displayhtml(settings.RECAPTCHA_PUBLIC_KEY, use_ssl=utils.use_ssl()))
 
     def value_from_datadict(self, data, files, name):
         return [data.get(self.recaptcha_challenge_name, None), 
