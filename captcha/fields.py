a9ea7f6d176248b57513b0c9a99e13fe75d75e41
Shaun Sephton <shaunsephton@gmail.com>
2011-08-10 10:46:37 +0200

    fetch ip from frame. todo ssl

--- captcha/fields.py
+++ captcha/fields.py
@@ -1,8 +1,11 @@
-from django.conf import settings
+import sys
+
 from django import forms
+from django.conf import settings
 from django.utils.encoding import smart_unicode
 from django.utils.translation import ugettext_lazy as _
 
+from captcha import utils
 from captcha.widgets import ReCaptcha
 
 from recaptcha.client import captcha
@@ -17,12 +20,22 @@ class ReCaptchaField(forms.CharField):
         self.required = True
         super(ReCaptchaField, self).__init__(*args, **kwargs)
 
+    def get_remote_ip(self):
+        f = sys._getframe()
+        while f:
+            if f.f_locals.has_key('request'):
+                request = f.f_locals['request']
+                if request:
+                    return request.META['REMOTE_ADDR']
+            f = f.f_back
+
     def clean(self, values):
         super(ReCaptchaField, self).clean(values[1])
         recaptcha_challenge_value = smart_unicode(values[0])
         recaptcha_response_value = smart_unicode(values[1])
-        check_captcha = captcha.submit(recaptcha_challenge_value, 
-            recaptcha_response_value, settings.RECAPTCHA_PRIVATE_KEY, {})
+        check_captcha = captcha.submit(recaptcha_challenge_value, recaptcha_response_value, settings.RECAPTCHA_PRIVATE_KEY, self.get_remote_ip())
+        # TODO: Use SSL.
+        #, use_ssl=utils.use_ssl())
         if not check_captcha.is_valid:
             raise forms.util.ValidationError(self.error_messages['captcha_invalid'])
         return values[0]
