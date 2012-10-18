5ce3cbd1cf0d3ca01d7920bca017e4dc302f3d5f
Shaun Sephton <shaun@28lines.com>
2012-10-18 16:50:34 +0200

    new db setting structure

--- test_settings.py
+++ test_settings.py
@@ -1,5 +1,12 @@
 DATABASE_ENGINE = 'sqlite3'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.sqlite',
+    }
+}
+
 INSTALLED_APPS = [
     'captcha',
 ]
