d8a43fee9ebd39bf368ce234c5d602ddc1a7b987
Peter <peter@praekelt.com>
2017-02-13 11:38:18 +0200

    Removed tox

--- .tox/py27-1.9/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/peter/praekelt/django-recaptcha/.tox/py27-1.9/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- .tox/py35-1.9/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/peter/praekelt/django-recaptcha/.tox/py35-1.9/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
