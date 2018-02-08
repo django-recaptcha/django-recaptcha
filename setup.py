30e1f3b27f4b69dee59f222a9c966cc6c2d8c2d5
Phala Ramonyai <phalamag@gmail.com>
2018-02-08 17:54:19 +0200

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_desc = open('README.rst', 'rb').read().decode('utf-8') + '\n\n' + \
             open('CHANGELOG.rst', 'rb').read().decode('utf-8')
 setup(
     name='django-recaptcha',
-    version='1.3.1',
+    version='1.4.0',
     description='Django recaptcha form field/widget app.',
     long_description=long_desc,
     author='Praekelt Consulting',
