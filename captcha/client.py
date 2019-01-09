5097bc91f7f85674a4992fd9e5898b2e5efe3eeb
AltusBarry <AltusBarry@users.noreply.github.com>
2019-01-09 15:04:45 +0200

    feature/remove-v1-endpoints: Removed NOCAPTCHA setting, updated code to no longer support reCAPTCH v1 widget, removed CAPTCHA_AJAX support

--- captcha/client.py
+++ captcha/client.py
@@ -10,24 +10,15 @@ from captcha.decorators import generic_deprecation
 DEFAULT_API_SSL_SERVER = "//www.google.com/recaptcha/api"  # made ssl agnostic
 DEFAULT_API_SERVER = "//www.google.com/recaptcha/api"  # made ssl agnostic
 DEFAULT_VERIFY_SERVER = "www.google.com"
-if getattr(settings, "NOCAPTCHA", False):
-    DEFAULT_WIDGET_TEMPLATE = 'captcha/widget_nocaptcha.html'
-else:
-    DEFAULT_WIDGET_TEMPLATE = 'captcha/widget.html'
-DEFAULT_WIDGET_TEMPLATE_AJAX = 'captcha/widget_ajax.html'
+DEFAULT_WIDGET_TEMPLATE = 'captcha/widget_nocaptcha.html'
 
 API_SSL_SERVER = getattr(settings, "CAPTCHA_API_SSL_SERVER",
                          DEFAULT_API_SSL_SERVER)
 API_SERVER = getattr(settings, "CAPTCHA_API_SERVER", DEFAULT_API_SERVER)
 VERIFY_SERVER = getattr(settings, "CAPTCHA_VERIFY_SERVER",
                         DEFAULT_VERIFY_SERVER)
-
-if getattr(settings, "CAPTCHA_AJAX", False):
-    WIDGET_TEMPLATE = getattr(settings, "CAPTCHA_WIDGET_TEMPLATE",
-                              DEFAULT_WIDGET_TEMPLATE_AJAX)
-else:
-    WIDGET_TEMPLATE = getattr(settings, "CAPTCHA_WIDGET_TEMPLATE",
-                              DEFAULT_WIDGET_TEMPLATE)
+WIDGET_TEMPLATE = getattr(settings, "CAPTCHA_WIDGET_TEMPLATE",
+    DEFAULT_WIDGET_TEMPLATE)
 
 
 RECAPTCHA_SUPPORTED_LANUAGES = ('en', 'nl', 'fr', 'de', 'pt', 'ru', 'es', 'tr')
@@ -55,10 +46,6 @@ def request(*args, **kwargs):
         return urlopen(*args, **kwargs)
 
 
-@generic_deprecation(
-    "reCAPTCHA v1 will no longer be supported."
-    " See NOCAPTCHA settings documentation and ensure the value is set to True."
-)
 def submit(recaptcha_challenge_field,
            recaptcha_response_field,
            private_key,
@@ -83,31 +70,15 @@ def submit(recaptcha_challenge_field,
             error_code='incorrect-captcha-sol'
         )
 
-    if getattr(settings, "NOCAPTCHA", False):
-        params = urlencode({
-            'secret': want_bytes(private_key),
-            'response': want_bytes(recaptcha_response_field),
-            'remoteip': want_bytes(remoteip),
-        })
-    else:
-        params = urlencode({
-            'privatekey': want_bytes(private_key),
-            'remoteip':  want_bytes(remoteip),
-            'challenge':  want_bytes(recaptcha_challenge_field),
-            'response':  want_bytes(recaptcha_response_field),
-        })
-
+    params = urlencode({
+        'secret': want_bytes(private_key),
+        'response': want_bytes(recaptcha_response_field),
+        'remoteip': want_bytes(remoteip),
+    })
     if not PY2:
         params = params.encode('utf-8')
 
-    if use_ssl:
-        verify_url = 'https://%s/recaptcha/api/verify' % VERIFY_SERVER
-    else:
-        verify_url = 'http://%s/recaptcha/api/verify' % VERIFY_SERVER
-
-    if getattr(settings, "NOCAPTCHA", False):
-        verify_url = 'https://%s/recaptcha/api/siteverify' % VERIFY_SERVER
-
+    verify_url = 'https://%s/recaptcha/api/siteverify' % VERIFY_SERVER
     req = Request(
         url=verify_url,
         data=params,
@@ -118,18 +89,13 @@ def submit(recaptcha_challenge_field,
     )
 
     httpresp = request(req)
-    if getattr(settings, "NOCAPTCHA", False):
-        data = json.loads(httpresp.read().decode('utf-8'))
-        return_code = data['success']
-        return_values = [return_code, None]
-        if return_code:
-            return_code = 'true'
-        else:
-            return_code = 'false'
+    data = json.loads(httpresp.read().decode('utf-8'))
+    return_code = data['success']
+    return_values = [return_code, None]
+    if return_code:
+        return_code = 'true'
     else:
-        return_values = httpresp.read().decode('utf-8').splitlines()
-        return_code = return_values[0]
-
+        return_code = 'false'
     httpresp.close()
 
     if (return_code == "true"):
--- captcha/tests/tests.py
+++ captcha/tests/tests.py
@@ -18,37 +18,23 @@ class TestForm(Form):
 
 
 class TestCase(TestCase):
-    # No longer supports reCAPTCHA v1, override settings during tests to always
-    # use v2 reCAPTCHA. Prevents HTTP 410 error.
-    @override_settings(NOCAPTCHA=True)
     @patch("captcha.fields.client.submit")
     def test_client_success_response(self, mocked_submit):
         mocked_submit.return_value = RecaptchaResponse(is_valid=True)
-        form_params = {'recaptcha_response_field': 'PASSED'}
+        form_params = {'g-recaptcha-response': 'PASSED'}
         form = TestForm(form_params)
         self.assertTrue(form.is_valid())
 
-    # No longer supports reCAPTCHA v1, override settings during tests to always
-    # use v2 reCAPTCHA. Prevents HTTP 410 error.
-    @override_settings(NOCAPTCHA=True)
     @patch("captcha.fields.client.submit")
     def test_client_failure_response(self, mocked_submit):
         mocked_submit.return_value = RecaptchaResponse(is_valid=False, error_code="410")
-        form_params = {'recaptcha_response_field': 'PASSED'}
+        form_params = {'g-recaptcha-response': 'PASSED'}
         form = TestForm(form_params)
         self.assertFalse(form.is_valid())
 
-    # No longer supports reCAPTCHA v1, override settings during tests to always
-    # use v2 reCAPTCHA. Prevents HTTP 410 error.
-    @override_settings(NOCAPTCHA=True)
     def test_deprecation_warning(self):
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
-            form_params = {'recaptcha_response_field': 'PASSED'}
-            form = TestForm(form_params)
+       form_params = {'g-recaptcha-response': 'PASSED'}
+       form = TestForm(form_params)
 
-            # Trigger warning on client.submit
-            form.is_valid()
-            assert len(w) == 1
-            assert issubclass(w[-1].category, DeprecationWarning)
-            assert "reCAPTCHA v1 will no longer be" in str(w[-1].message)
+       # Trigger client.submit
+       form.is_valid()
