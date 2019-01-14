aba942ab9fba105b6d6bc9cf981434303a43c960
AltusBarry <AltusBarry@users.noreply.github.com>
2019-01-14 10:54:17 +0200

    feature/client-testing: Added test file

---
+++ captcha/tests/tests.py
@@ -0,0 +1,45 @@
+try:
+    from importlib import reload
+except ImportError:
+    pass
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.test import TestCase, override_settings
+
+import captcha
+
+
+class TestInit(TestCase):
+
+    def test_setting_instance_check(self):
+        with override_settings(RECAPTCHA_PROXY="not a dict"):
+            with self.assertRaises(ImproperlyConfigured) as error:
+                reload(captcha)
+            self.assertEqual(error.exception.args, (
+                "Setting RECAPTCHA_PROXY is not of type", dict)
+            )
+        with override_settings(RECAPTCHA_VERIFY_REQUEST_TIMEOUT="not an int"):
+            with self.assertRaises(ImproperlyConfigured) as error:
+                reload(captcha)
+            self.assertEqual(error.exception.args, (
+                "Setting RECAPTCHA_VERIFY_REQUEST_TIMEOUT is not of type", int)
+            )
+        with override_settings(RECAPTCHA_VERIFY_ENDPOINT=1):
+            with self.assertRaises(ImproperlyConfigured) as error:
+                reload(captcha)
+            self.assertEqual(error.exception.args, (
+                "Setting RECAPTCHA_VERIFY_ENDPOINT is not of type", str)
+            )
+        with override_settings(RECAPTCHA_PUBLIC_KEY=1):
+            with self.assertRaises(ImproperlyConfigured) as error:
+                reload(captcha)
+            self.assertEqual(error.exception.args, (
+                "Setting RECAPTCHA_PUBLIC_KEY is not of type", str)
+            )
+        with override_settings(RECAPTCHA_PRIVATE_KEY=1):
+            with self.assertRaises(ImproperlyConfigured) as error:
+                reload(captcha)
+            self.assertEqual(error.exception.args, (
+                "Setting RECAPTCHA_PRIVATE_KEY is not of type", str)
+            )
