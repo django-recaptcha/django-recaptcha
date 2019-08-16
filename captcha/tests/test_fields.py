0c154360c5d2a668e56b0202431e1ba2c55967c5
Victor Carlquist <victorcarlquist@gmail.com>
2019-08-16 18:03:36 -0300

    add test to score-threshold

--- captcha/tests/test_fields.py
+++ captcha/tests/test_fields.py
@@ -321,3 +321,42 @@ class TestWidgets(TestCase):
             '?render=pubkey"></script>',
             html
         )
+
+    @patch("captcha.fields.client.submit")
+    def test_client_success_response_v3(self, mocked_submit):
+        class VThreeDomainForm(forms.Form):
+            captcha = fields.ReCaptchaField(
+                widget=widgets.ReCaptchaV3(attrs={'score-threshold': 0.8})
+            )
+        mocked_submit.return_value = RecaptchaResponse(
+            is_valid=True,
+            extra_data={'score': 0.9})
+        form_params = {"captcha": "PASSED"}
+        form = VThreeDomainForm(form_params)
+        self.assertTrue(form.is_valid())
+
+    @patch("captcha.fields.client.submit")
+    def test_client_failure_response_v3(self, mocked_submit):
+        class VThreeDomainForm(forms.Form):
+            captcha = fields.ReCaptchaField(
+                widget=widgets.ReCaptchaV3(attrs={'score-threshold': 0.8})
+            )
+        mocked_submit.return_value = RecaptchaResponse(
+            is_valid=True,
+            extra_data={'score': 0.1})
+        form_params = {"captcha": "PASSED"}
+        form = VThreeDomainForm(form_params)
+        self.assertFalse(form.is_valid())
+
+    @patch("captcha.fields.client.submit")
+    def test_client_empty_score_threshold_v3(self, mocked_submit):
+        class VThreeDomainForm(forms.Form):
+            captcha = fields.ReCaptchaField(
+                widget=widgets.ReCaptchaV3()
+            )
+        mocked_submit.return_value = RecaptchaResponse(
+            is_valid=True,
+            extra_data={'score': 0.1})
+        form_params = {"captcha": "PASSED"}
+        form = VThreeDomainForm(form_params)
+        self.assertTrue(form.is_valid())
