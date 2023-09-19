14934d0229e02fcba1f99f7af4dab3d8ed797cb9
Alex Robertson <131001059+alexrobertson-muckrack@users.noreply.github.com>
2023-09-19 08:25:15 -0600

    Feature Request: Add actions in recaptcha v3 widget (#304)
    * added reCAPTCHAv3 action to widget
    
    * added missing }
    
    * Bump
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: Binoy Udayan <CC-AEC_EP-0061@configcar.onmicrosoft.de>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- captcha/widgets.py
+++ captcha/widgets.py
@@ -70,12 +70,13 @@ class ReCaptchaV2Invisible(ReCaptchaBase):
 class ReCaptchaV3(ReCaptchaBase):
     template_name = "captcha/widget_v3.html"
 
-    def __init__(self, api_params=None, *args, **kwargs):
+    def __init__(self, api_params=None, action="form", *args, **kwargs):
         super().__init__(api_params=api_params, *args, **kwargs)
         if not self.attrs.get("required_score", None):
             self.attrs["required_score"] = getattr(
                 settings, "RECAPTCHA_REQUIRED_SCORE", None
             )
+        self.action = action
 
     def build_attrs(self, base_attrs, extra_attrs=None):
         attrs = super().build_attrs(base_attrs, extra_attrs)
@@ -83,3 +84,8 @@ class ReCaptchaV3(ReCaptchaBase):
 
     def value_from_datadict(self, data, files, name):
         return data.get(name)
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context.update({"action": self.action})
+        return context
