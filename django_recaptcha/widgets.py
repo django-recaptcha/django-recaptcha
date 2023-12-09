27106d7278efd914e12c729ea4f8ccb3453940f7
Alan Verresen <dev@alanverresen.com>
2023-12-10 00:39:15 +0100

    Use static template tag to include scripts.

--- django_recaptcha/widgets.py
+++ django_recaptcha/widgets.py
@@ -4,7 +4,6 @@ from urllib.parse import urlencode
 
 from django.conf import settings
 from django.forms import widgets
-from django.templatetags.static import static
 
 from django_recaptcha.constants import DEFAULT_RECAPTCHA_DOMAIN
 
@@ -42,7 +41,6 @@ class ReCaptchaBase(widgets.Widget):
                 "recaptcha_domain": getattr(
                     settings, "RECAPTCHA_DOMAIN", DEFAULT_RECAPTCHA_DOMAIN
                 ),
-                "script_url": self.script_name,
             }
         )
         return context
@@ -62,13 +60,11 @@ class ReCaptchaBase(widgets.Widget):
 class ReCaptchaV2Checkbox(ReCaptchaBase):
     input_type = "hidden"
     template_name = "django_recaptcha/widget_v2_checkbox.html"
-    script_name = static("django_recaptcha/js/widget_v2_checkbox.js")
 
 
 class ReCaptchaV2Invisible(ReCaptchaBase):
     input_type = "hidden"
     template_name = "django_recaptcha/widget_v2_invisible.html"
-    script_name = static("django_recaptcha/js/widget_v2_invisible.js")
 
     def build_attrs(self, base_attrs, extra_attrs=None):
         attrs = super().build_attrs(base_attrs, extra_attrs)
@@ -81,7 +77,6 @@ class ReCaptchaV2Invisible(ReCaptchaBase):
 class ReCaptchaV3(ReCaptchaBase):
     input_type = "hidden"
     template_name = "django_recaptcha/widget_v3.html"
-    script_name = static("django_recaptcha/js/widget_v3.js")
 
     def __init__(
         self, api_params=None, action=None, required_score=None, *args, **kwargs
