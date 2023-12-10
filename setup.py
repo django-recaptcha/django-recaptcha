2cf1a111ac62f866bba1c12ace0823a8d4eeb34b
Storm B. Heg <storm@stormbase.digital>
2023-12-10 14:15:35 +0100

    Declare official support for Django 5.0 and Python 3.12

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.1",
         "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
@@ -37,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     zip_safe=False,
 )
