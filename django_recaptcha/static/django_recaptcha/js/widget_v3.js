9e3154b9ea7c7e687ce9e735f2c94bccc210e718
Alan Verresen <dev@alanverresen.com>
2025-05-10 17:50:30 +0200

    Switched from IIFE to ES modules for widget scripts.

--- django_recaptcha/static/django_recaptcha/js/widget_v3.js
+++ django_recaptcha/static/django_recaptcha/js/widget_v3.js
@@ -1,53 +1,49 @@
-(function () {
+function main() {
+  grecaptcha.ready(function () {
+    for (const captchaElement of document.querySelectorAll(".g-recaptcha")) {
 
-  function main() {
-    grecaptcha.ready(function () {
-      for (const captchaElement of document.querySelectorAll(".g-recaptcha")) {
-
-        const widgetUuid = captchaElement.getAttribute("data-widget-uuid");
-        if (!widgetUuid) {
-          console.warn("reCAPTCHA widget with missing UUID");
-          break;
-        }
-        console.log(`found reCAPTCHA widget with UUID '${widgetUuid}'`);
-
-        const formElement = captchaElement.closest("form");
-        if (!formElement) {
-          console.warn(`reCAPTCHA widget with UUID '${widgetUuid}' is not part of a form`);
-          break;
-        }
-
-        const publicKey = captchaElement.getAttribute("data-sitekey");
-        if (publicKey === null) {
-          console.warn(`public key missing missing from reCAPTCHA widget with UUID '${widgetUuid}'`);
-          break;
-        }
-
-        const actionName = captchaElement.getAttribute("data-action");
-        const config = {};
-        if (actionName !== null) {
-          config.action = actionName;
-        }
-
-        formElement.addEventListener("submit", function (event) {
-          event.preventDefault();
-          grecaptcha.execute(publicKey, config)
-            .then(function (token) {
-              console.log(`reCAPTCHA validated for reCAPTCHA widget with UUID '${widgetUuid}'`);
-              captchaElement.value = token;
-              console.log("submitting form...");
-              formElement.submit();
-            });
-        });
-
+      const widgetUuid = captchaElement.getAttribute("data-widget-uuid");
+      if (!widgetUuid) {
+        console.warn("reCAPTCHA widget with missing UUID");
+        break;
       }
-    });
-  }
+      console.log(`found reCAPTCHA widget with UUID '${widgetUuid}'`);
 
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", main);
-  } else {
-    main();
-  }
+      const formElement = captchaElement.closest("form");
+      if (!formElement) {
+        console.warn(`reCAPTCHA widget with UUID '${widgetUuid}' is not part of a form`);
+        break;
+      }
 
-})();
+      const publicKey = captchaElement.getAttribute("data-sitekey");
+      if (publicKey === null) {
+        console.warn(`public key missing missing from reCAPTCHA widget with UUID '${widgetUuid}'`);
+        break;
+      }
+
+      const actionName = captchaElement.getAttribute("data-action");
+      const config = {};
+      if (actionName !== null) {
+        config.action = actionName;
+      }
+
+      formElement.addEventListener("submit", function (event) {
+        event.preventDefault();
+        grecaptcha.execute(publicKey, config)
+          .then(function (token) {
+            console.log(`reCAPTCHA validated for reCAPTCHA widget with UUID '${widgetUuid}'`);
+            captchaElement.value = token;
+            console.log("submitting form...");
+            formElement.submit();
+          });
+      });
+
+    }
+  });
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", main);
+} else {
+  main();
+}
