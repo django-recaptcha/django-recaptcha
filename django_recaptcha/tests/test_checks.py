2bf16d3455f4b35b5936d7645c9f826be3932331
Alan Verresen <dev@alanverresen.com>
2025-11-29 09:33:58 +0100

    Add missing tests for checking test key usage.

--- django_recaptcha/tests/test_checks.py
+++ django_recaptcha/tests/test_checks.py
@@ -1,8 +1,9 @@
 from django.test import TestCase, override_settings
 
-from django.core.checks import Error
 import django_recaptcha
+from django_recaptcha.checks import recaptcha_key_check
 from django_recaptcha.constants import TEST_PRIVATE_KEY
+from django_recaptcha.tests.settings.coveralls_settings import RECAPTCHA_PUBLIC_KEY
 
 
 class TestChecks(TestCase):
@@ -35,10 +36,21 @@ class TestChecks(TestCase):
 
         self.assertEqual(1, len(relevant_errs))
 
-    @override_settings(
-        DEBUG=False,
-        RECAPTCHA_PRIVATE_KEY=TEST_PRIVATE_KEY,
-    )
+    @override_settings(DEBUG=True, RECAPTCHA_PRIVATE_KEY=TEST_PRIVATE_KEY)
+    def test_check_test_key_usage_skipped(self):
+        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
+        relevant_errs = [e for e in errs if e.id == "django_recaptcha.recaptcha_test_key_error"]
+
+        self.assertEqual(0, len(relevant_errs))
+
+    @override_settings(DEBUG=False, RECAPTCHA_PRIVATE_KEY="privatekey", RECAPTCHA_PUBLIC_KEY="publickey")
+    def test_check_test_key_usage_passes(self):
+        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
+        relevant_errs = [e for e in errs if e.id == "django_recaptcha.recaptcha_test_key_error"]
+
+        self.assertEqual(0, len(relevant_errs))
+
+    @override_settings(DEBUG=False, RECAPTCHA_PRIVATE_KEY=TEST_PRIVATE_KEY)
     def test_check_test_key_usage_fails(self):
         errs = django_recaptcha.checks.recaptcha_key_check("someconf")
         relevant_errs = [e for e in errs if e.id == "django_recaptcha.recaptcha_test_key_error"]
