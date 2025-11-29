cc93a9c708ed7de03f58413cd2c776382f3fc0a5
Alan Verresen <dev@alanverresen.com>
2025-11-29 09:11:04 +0100

    Rewrite test for checking test key usage.

--- django_recaptcha/tests/test_checks.py
+++ django_recaptcha/tests/test_checks.py
@@ -2,6 +2,7 @@ from django.test import TestCase, override_settings
 
 from django.core.checks import Error
 import django_recaptcha
+from django_recaptcha.constants import TEST_PRIVATE_KEY
 
 
 class TestChecks(TestCase):
@@ -35,22 +36,11 @@ class TestChecks(TestCase):
         self.assertEqual(1, len(relevant_errs))
 
     @override_settings(
-        RECAPTCHA_PRIVATE_KEY=django_recaptcha.constants.TEST_PRIVATE_KEY
+        DEBUG=False,
+        RECAPTCHA_PRIVATE_KEY=TEST_PRIVATE_KEY,
     )
-    def test_test_key_check(self):
-        check_errors = django_recaptcha.checks.recaptcha_key_check("someconf")
-        expected_errors = [
-            Error(
-                "RECAPTCHA_PRIVATE_KEY or RECAPTCHA_PUBLIC_KEY is making use"
-                " of the Google test keys and will not behave as expected in a"
-                " production environment",
-                hint="Update settings.RECAPTCHA_PRIVATE_KEY"
-                " and/or settings.RECAPTCHA_PUBLIC_KEY. Alternatively this"
-                " check can be ignored by adding"
-                " `SILENCED_SYSTEM_CHECKS ="
-                " ['django_recaptcha.recaptcha_test_key_error']`"
-                " to your settings file.",
-                id="django_recaptcha.recaptcha_test_key_error",
-            )
-        ]
-        self.assertEqual(check_errors, expected_errors)
+    def test_check_test_key_usage_fails(self):
+        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
+        relevant_errs = [e for e in errs if e.id == "django_recaptcha.recaptcha_test_key_error"]
+
+        self.assertEqual(1, len(relevant_errs))
