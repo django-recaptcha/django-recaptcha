300c630d55dc1b2015ba703be4cacb56c66924d6
Alan Verresen <dev@alanverresen.com>
2025-11-29 09:44:12 +0100

    Refactor tests for checking key usage.

--- django_recaptcha/tests/test_checks.py
+++ django_recaptcha/tests/test_checks.py
@@ -1,58 +1,61 @@
 from django.test import TestCase, override_settings
 
-import django_recaptcha
 from django_recaptcha.checks import recaptcha_key_check
 from django_recaptcha.constants import TEST_PRIVATE_KEY
-from django_recaptcha.tests.settings.coveralls_settings import RECAPTCHA_PUBLIC_KEY
+
+
+def _filter_errs(errs, err_id):
+    """Returns filtered list with relevant error."""
+    return [e for e in errs if e.id == err_id]
 
 
 class TestChecks(TestCase):
 
     @override_settings(RECAPTCHA_PRIVATE_KEY="notempty")
     def test_check_private_key_is_empty_passes(self):
-        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
-        relevant_errs = [e for e in errs if e.id == "django_recaptcha.private_key_is_empty_string"]
+        errs = recaptcha_key_check("someconf")
+        errs = _filter_errs(errs, "django_recaptcha.private_key_is_empty_string")
 
-        self.assertEqual(0, len(relevant_errs))
+        self.assertEqual(0, len(errs))
 
     @override_settings(RECAPTCHA_PRIVATE_KEY="")
     def test_check_private_key_is_empty_fails(self):
-        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
-        relevant_errs = [e for e in errs if e.id == "django_recaptcha.private_key_is_empty_string"]
+        errs = recaptcha_key_check("someconf")
+        errs = _filter_errs(errs, "django_recaptcha.private_key_is_empty_string")
 
-        self.assertEqual(1, len(relevant_errs))
+        self.assertEqual(1, len(errs))
 
     @override_settings(RECAPTCHA_PUBLIC_KEY="notempty")
     def test_check_public_key_is_empty_passes(self):
-        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
-        relevant_errs = [e for e in errs if e.id == "django_recaptcha.public_key_is_empty_string"]
+        errs = recaptcha_key_check("someconf")
+        errs = _filter_errs(errs, "django_recaptcha.public_key_is_empty_string")
 
-        self.assertEqual(0, len(relevant_errs))
+        self.assertEqual(0, len(errs))
 
     @override_settings(RECAPTCHA_PUBLIC_KEY="")
     def test_check_public_key_is_empty_fails(self):
-        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
-        relevant_errs = [e for e in errs if e.id == "django_recaptcha.public_key_is_empty_string"]
+        errs = recaptcha_key_check("someconf")
+        errs = _filter_errs(errs, "django_recaptcha.public_key_is_empty_string")
 
-        self.assertEqual(1, len(relevant_errs))
+        self.assertEqual(1, len(errs))
 
     @override_settings(DEBUG=True, RECAPTCHA_PRIVATE_KEY=TEST_PRIVATE_KEY)
     def test_check_test_key_usage_skipped(self):
-        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
-        relevant_errs = [e for e in errs if e.id == "django_recaptcha.recaptcha_test_key_error"]
+        errs = recaptcha_key_check("someconf")
+        errs = _filter_errs(errs, "django_recaptcha.recaptcha_test_key_error")
 
-        self.assertEqual(0, len(relevant_errs))
+        self.assertEqual(0, len(errs))
 
     @override_settings(DEBUG=False, RECAPTCHA_PRIVATE_KEY="privatekey", RECAPTCHA_PUBLIC_KEY="publickey")
     def test_check_test_key_usage_passes(self):
-        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
-        relevant_errs = [e for e in errs if e.id == "django_recaptcha.recaptcha_test_key_error"]
+        errs = recaptcha_key_check("someconf")
+        errs = _filter_errs(errs, "django_recaptcha.recaptcha_test_key_error")
 
-        self.assertEqual(0, len(relevant_errs))
+        self.assertEqual(0, len(errs))
 
     @override_settings(DEBUG=False, RECAPTCHA_PRIVATE_KEY=TEST_PRIVATE_KEY)
     def test_check_test_key_usage_fails(self):
-        errs = django_recaptcha.checks.recaptcha_key_check("someconf")
-        relevant_errs = [e for e in errs if e.id == "django_recaptcha.recaptcha_test_key_error"]
+        errs = recaptcha_key_check("someconf")
+        errs = _filter_errs(errs, "django_recaptcha.recaptcha_test_key_error")
 
-        self.assertEqual(1, len(relevant_errs))
+        self.assertEqual(1, len(errs))
